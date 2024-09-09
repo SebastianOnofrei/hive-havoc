f3a1c9e451fb5fc4946ce7900e5a6fffd5dd6566
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2024-09-09 19:00:09 +0300

    feat: css cleanup and better structuring

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-// We put our JS here so we wait for all the assets to be loaded
+// We put our JS here so we wait for all the assets to be loaded before doing anything
 
 window.addEventListener("load", function () {
   // initializing canvas
   const canvas = document.getElementById("canvas1");
   const context = canvas.getContext("2d");
-  canvas.width = 600;
-  canvas.height = 600;
+  canvas.width = 1440;
+  canvas.height = 1080;
+
+  // dom elements
+  const hitBtn = document.querySelector(".hit-button");
+
+  hitBtn.addEventListener("click", (e) => {
+    console.log(e);
+    console.log("Pressed button");
+  });
   // Loading music
   const audio = document.getElementById("background-music");
 
@@ -18,14 +26,13 @@ window.addEventListener("load", function () {
   const backgrounds = [autumnBg, springBg, summerBg, winterBg];
   let currentIndex = 0;
   let backgroundImage = new Image();
-  console.log(backgrounds);
+
   // individual sprites (only when one type remaining on each)
   const workerBee = document.getElementById("worker-bee");
   const droneBee = document.getElementById("drone-bee");
   const queenBee = document.getElementById("queen-bee");
 
   // swarm sprites
-
   const droneBeesSwarm = document.getElementById("drone-bees-swarm");
   const workerBeesSwarm = document.getElementById("worker-bees-swarm");
 
@@ -33,11 +40,13 @@ window.addEventListener("load", function () {
     backgroundImage.src = backgrounds[currentIndex].src;
     backgroundImage.onload = function () {
       context.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+
       context.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height); // Draw new background
 
-      context.drawImage(workerBeesSwarm, 50, 200, 150, 150);
-      context.drawImage(queenBee, 200, 100, 150, 150);
-      context.drawImage(droneBeesSwarm, 300, 200, 150, 150);
+      context.drawImage(workerBeesSwarm, 340, 400, 350, 350);
+      context.drawImage(queenBee, 600, 200, 350, 350);
+
+      context.drawImage(droneBeesSwarm, 800, 400, 350, 350);
     };
 
     currentIndex = (currentIndex + 1) % backgrounds.length; // Increment index and loop back if needed
@@ -45,16 +54,17 @@ window.addEventListener("load", function () {
 
   // Initial call to display the first background
   changeBackground();
-  // Set Interval to change background every 8 seconds
-  setInterval(changeBackground, 30000);
-  class Game {
-    constructor(width, height) {
-      this.width = width;
-      this.height = height;
-    }
 
-    update() {}
-    draw() {}
-  }
-  audio.play();
+  // Set Interval to change background every 8 seconds
+  // setInterval(changeBackground, 30000);
+  // class Game {
+  //   constructor(width, height) {
+  //     this.width = width;
+  //     this.height = height;
+  //   }
+
+  //   update() {}
+  //   draw() {}
+  // }
+  // audio.play();
 });
