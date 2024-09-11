7e29076cbefdd87a255f1bda4f820d0fcf7d6ba4
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2024-09-11 21:08:10 +0300

    feat: refactored the intro Scene.

--- js/scenes/IntroScene.js
+++ js/scenes/IntroScene.js
@@ -8,33 +8,6 @@ export class IntroScene extends BaseScene {
 
     this.width = 0;
     this.height = 0;
-
-    // We need a scaling factor for sprite responsiveness to resize event.
-    this.scalingFactor = 1;
-    this.droneBeePositions = {
-      x: 0,
-      y: 0,
-    };
-    this.droneBeesSwarmPositions = {
-      x: 0,
-      y: 0,
-    };
-
-    this.workerBeePositions = {
-      x: 0,
-      y: 0,
-    };
-
-    this.workerBeesSwarmPositions = {
-      x: 0,
-      y: 0,
-    };
-
-    this.queenBeePositions = {
-      x: 0,
-      y: 0,
-    };
-
     this.buttonsSettings = {
       buttonWidth: 0,
       buttonHeight: 0,
@@ -43,77 +16,12 @@ export class IntroScene extends BaseScene {
       settingsButtonY: 0,
     };
 
-    // DOM Selectors
     this.honeyBg = document.getElementById("honey-bg");
-    this.workerBee = document.getElementById("worker-bee");
-    this.droneBee = document.getElementById("drone-bee");
-    this.droneBeesSwarm = document.getElementById("drone-bees-swarm");
-    this.workerBeesSwarm = document.getElementById("worker-bees-swarm");
-    this.queenBee = document.getElementById("queen-bee");
-  }
-
-  // for resizing the window
-  changeSpritePositions(width) {
-    this.workerBeePositions.x = width * 0.1;
-    this.workerBeePositions.y = 20;
-
-    this.droneBeePositions.x = width * 0.7;
-    this.droneBeePositions.y = 20;
-
-    this.droneBeesSwarmPositions.x = width * 0.1;
-    this.droneBeesSwarmPositions.y = 420;
-
-    this.workerBeesSwarmPositions.x = width * 0.7;
-    this.workerBeesSwarmPositions.y = 420;
-
-    this.queenBeePositions.x = width * 0.45;
-    this.queenBeePositions.y = 480;
-
-    if (width > 1400) {
-      this.scalingFactor = 1.4;
-    } else if (width <= 1400 && width > 1200) {
-      this.scalingFactor = 1.2;
-    } else if (width <= 1200 && width > 1000) {
-      this.scalingFactor = 1;
-
-      this.workerBeePositions.x = width * 0.1;
-      this.workerBeePositions.y = 50;
-
-      this.droneBeePositions.x = width * 0.8;
-      this.droneBeePositions.y = 50;
-
-      this.droneBeesSwarmPositions.x = width * 0.1;
-      this.droneBeesSwarmPositions.y = 420;
-
-      this.workerBeesSwarmPositions.x = width * 0.7;
-      this.workerBeesSwarmPositions.y = 420;
-
-      this.queenBeePositions.x = width * 0.42;
-      this.queenBeePositions.y = 450;
-    } else if (width <= 1000) {
-      this.scalingFactor = 0.9;
-
-      this.workerBeePositions.x = width * 0.1;
-      this.workerBeePositions.y = 150;
-
-      this.droneBeePositions.x = width * 0.8;
-      this.droneBeePositions.y = 150;
-
-      this.droneBeesSwarmPositions.x = width * 0.05;
-      this.droneBeesSwarmPositions.y = 420;
-
-      this.workerBeesSwarmPositions.x = width * 0.65;
-      this.workerBeesSwarmPositions.y = 420;
-
-      this.queenBeePositions.x = width * 0.42;
-      this.queenBeePositions.y = 450;
-    }
   }
 
   draw(ctx) {
     super.draw(ctx); // Call the base class draw method to clear the canvas
-    // drawing the sprites and buttons
-    this.drawSprites(ctx);
+    this.drawBg(ctx);
     this.drawButtons(ctx);
   }
 
@@ -137,49 +45,11 @@ export class IntroScene extends BaseScene {
     }
   }
 
-  drawSprites(ctx) {
+  drawBg(ctx) {
     // hive background
     let backgroundImage = new Image();
     backgroundImage.src = this.honeyBg.src;
-
-    // in functie de ce width am, sa am un scaling factor si pozitii diferite.
-    this.changeSpritePositions(this.width);
     ctx.drawImage(backgroundImage, 0, 0, this.width, this.height);
-    ctx.drawImage(
-      this.workerBee,
-      this.workerBeePositions.x,
-      this.workerBeePositions.y,
-      160 * this.scalingFactor,
-      160 * this.scalingFactor
-    );
-    ctx.drawImage(
-      this.droneBee,
-      this.droneBeePositions.x,
-      this.droneBeePositions.y,
-      160 * this.scalingFactor,
-      160 * this.scalingFactor
-    );
-    ctx.drawImage(
-      this.droneBeesSwarm,
-      this.droneBeesSwarmPositions.x,
-      this.droneBeesSwarmPositions.y,
-      260 * this.scalingFactor,
-      260 * this.scalingFactor
-    );
-    ctx.drawImage(
-      this.workerBeesSwarm,
-      this.workerBeesSwarmPositions.x,
-      this.workerBeesSwarmPositions.y,
-      260 * this.scalingFactor,
-      260 * this.scalingFactor
-    );
-    ctx.drawImage(
-      this.queenBee,
-      this.queenBeePositions.x,
-      this.queenBeePositions.y,
-      160 * this.scalingFactor,
-      160 * this.scalingFactor
-    );
   }
 
   drawButtons(ctx) {
@@ -204,9 +74,13 @@ export class IntroScene extends BaseScene {
     };
 
     if (this.width > 1400) {
-      ctx.font = "72px Segoe Script";
+      ctx.font = "84px Segoe Script";
+    } else if (this.width <= 1000 && this.width > 700) {
+      ctx.font = "52px Segoe Script";
+    } else if (this.width < 700) {
+      ctx.font = "48px Segoe Script";
     } else {
-      ctx.font = "60px Segoe Script";
+      ctx.font = "64px Segoe Script";
     }
 
     ctx.fillStyle = "black";
